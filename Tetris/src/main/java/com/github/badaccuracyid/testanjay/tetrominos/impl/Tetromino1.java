c288fb53b6984c576a9b29b55c38f0acc4046c07
BadAccuracyID <37508513+BadAccuracyID@users.noreply.github.com>
2022-10-16 23:05:49 +0700

    Fixed Tetromino1 default x position

--- Tetris/src/main/java/com/github/badaccuracyid/testanjay/tetrominos/impl/Tetromino1.java
+++ Tetris/src/main/java/com/github/badaccuracyid/testanjay/tetrominos/impl/Tetromino1.java
@@ -6,7 +6,7 @@ public class Tetromino1 extends Tetromino {
 
     public Tetromino1() {
         this.rotation = 0;
-        this.x = 9;
+        this.x = 0;
         this.y = 0;
         this.shape = new char[][][]{
                 {
